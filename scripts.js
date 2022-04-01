fd6496baa9a0e121fee549bffcf813ecc396fba8
blayzeon <blayzeon@gmail.com>
2022-04-01 17:24:10 -0400

    add copy button functionality

--- scripts.js
+++ scripts.js
@@ -7,6 +7,10 @@
 
 const form = [
     {
+        opening: false,
+        closing: false
+    },
+    {
         type: 'textarea',
         placeholder: 'scratchpad for jotting down information (not included in notes)',
         copy: false,
@@ -23,14 +27,14 @@ const form = [
         label: 'issue',
         type: 'input',
         placeholder: 'the reasson for the customer\'s call',
-        copy: 'value',
+        copy: true,
         reset: true
     },
     {
         label: 'facility',
         type: 'input',
         placeholder: 'the inmate\'s facility',
-        copy: 'value',
+        copy: true,
         reset: true
     },
     {
@@ -50,15 +54,27 @@ const form = [
         label: 'resolution',
         type: 'input',
         placeholder: 'how you assisted the customer',
-        copy: 'value',
+        copy: true,
         reset: true
     },
-]
+];
+
+function copyStr(str){
+    const copyArea = document.createElement('textarea');
+    copyArea.value = str;
+    copyArea.setAttribute('readonly', '');
+    document.body.appendChild(copyArea);
+    copyArea.select();
+    document.execCommand('copy');
+    copyArea.remove();
+}
 
 function createForm(itemArray){
     const form = document.createElement('form');
 
     for (let i = 0; i < itemArray.length; i += 1){
+        if (!itemArray[i].type) { continue };
+
         const formItem = document.createElement('div');
         formItem.classList.add('form-group');
 
@@ -72,6 +88,11 @@ function createForm(itemArray){
         if (itemArray[i].reset){
             newInput.setAttribute('data', 'reset');
         }
+
+        if (itemArray[i].copy){
+            newInput.setAttribute('data', 'copy');
+        }
+
         if (itemArray[i].options){
             for (let j = 0; j < itemArray[i].options.length; j += 1){
                 const newOption = document.createElement('option');
@@ -94,6 +115,23 @@ function createForm(itemArray){
     copyBtn.setAttribute('type', 'button');
     copyBtn.innerText = 'copy';
     formButtons.appendChild(copyBtn);
+
+    copyBtn.addEventListener('click', ()=>{
+        const formValues = Array.from(document.querySelectorAll('[data="copy"]'))
+            .map((x) => {return x.value})
+            .filter((a) => a);
+        let noteString = formValues.join(' / ');
+
+        if (itemArray[0].opening){
+            noteString = `${itemArray[0].opening} ${noteString}`;
+        }
+
+        if (itemArray[0].closing){
+            noteString = `${noteString} ${itemArray[0].closing}`;
+        }
+
+        copyStr(noteString);
+    });
 
     const resetBtn = document.createElement('button');
     resetBtn.setAttribute('type', 'button');
